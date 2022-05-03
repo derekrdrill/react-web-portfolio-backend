71c6429c71e054dea15ce654a77589e721878147
Derek Drill <derekrdrill@gmail.com>
2022-05-03 10:38:30 -0400

    improvement to feedbackItems routes

--- routes/feedbackItems.js
+++ routes/feedbackItems.js
@@ -3,44 +3,42 @@ const recordRoutes = express.Router();
 const conn = require('../db/conn');
 const ObjectId = require('mongodb').ObjectId;
 
-recordRoutes.route('/feedbackItems').get(function (req, res) {
-   conn
-      .getDb()
-      .collection('feedbackItems')
-      .find({})
-      .toArray((err, result) => {
-         if (err) throw err;
-         res.json(result);
-      });
+recordRoutes.route('/feedbackItems').get(async (req, res) => {
+   const feedbackCollection = await conn.getDb().collection('feedbackItems');
+
+   await feedbackCollection.find({}).toArray((err, result) => {
+      if (err) throw err;
+      res.json(result);
+   });
 });
 
-recordRoutes.route('/addFeedbackItem').post(function (req) {
-   conn
-      .getDb()
-      .collection('feedbackItems')
-      .insert(req.body, err => {
-         if (err) throw err;
-      });
+recordRoutes.route('/addFeedbackItem').post(async (req, res) => {
+   let returnFeedbackData = {};
+
+   const feedbackCollection = await conn.getDb().collection('feedbackItems');
+   const feedbackCollectionInsertion = await feedbackCollection.insertOne(req.body);
+
+   if (feedbackCollectionInsertion.acknowledged) {
+      returnFeedbackData = await feedbackCollection.findOne({ _id: feedbackCollectionInsertion.insertedId });
+   }
+
+   res.send(returnFeedbackData);
 });
 
-recordRoutes.route('/deleteFeedbackItem').post(function (req) {
-   conn
-      .getDb()
-      .collection('feedbackItems')
-      .deleteOne({ _id: ObjectId(req.body.id) }, err => {
-         if (err) throw err;
-      });
+recordRoutes.route('/deleteFeedbackItem').post(async req => {
+   const feedbackCollection = await conn.getDb().collection('feedbackItems');
+   await feedbackCollection.deleteOne({ _id: ObjectId(req.body.id) }, err => {
+      if (err) throw err;
+   });
 });
 
-recordRoutes.route('/updateFeedbackItem').post(function (req) {
+recordRoutes.route('/updateFeedbackItem').post(async req => {
    let newFeedbackInfo = { ...req.body, ...{ _id: ObjectId(req.body._id) } };
+   const feedbackCollection = await conn.getDb().collection('feedbackItems');
 
-   conn
-      .getDb()
-      .collection('feedbackItems')
-      .replaceOne({ _id: newFeedbackInfo._id }, newFeedbackInfo, err => {
-         if (err) throw err;
-      });
+   await feedbackCollection.replaceOne({ _id: newFeedbackInfo._id }, newFeedbackInfo, err => {
+      if (err) throw err;
+   });
 });
 
 module.exports = recordRoutes;
